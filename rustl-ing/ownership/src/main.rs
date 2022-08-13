c6231c27b7e725b76249128533193f574dfd1e51
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-08-13 15:59:41 +0700

    Update main.rs

--- rustl-ing/ownership/src/main.rs
+++ rustl-ing/ownership/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     
     // String datatype
+    {
+        // let ss = "String Literal"; // cannot be mutated, stack mem
+        let mut s = String::from("Hello");
 
-    // let ss = "String Literal"; // cannot be mutated, stack mem
+        s.push_str(", world!");
 
-    let mut s = String::from("Hello");
-
-    s.push_str(", world!");
-
-    println!("{}", s);
+        println!("{}", s);
+    }
     
     // move - move s1 to s2, and s1 cannot be access anymore
     {
@@ -26,4 +26,72 @@ fn main() {
     
         println!("s1 = {}, s2 = {}", s1, s2);
     }
+
+    {
+        let s = String::from("hello"); // s comes into scope
+
+        takes_ownership(s);             // s's value moves into the function...
+                                                // ... and so is no longer valid here
+        let x = 5;
+
+        makes_copy(x);                  // x comes into scope
+                                                // x would move into the function,
+                                                // but i32 is Copy, so it's okay to still
+                                                // use x afterward
+    }   // Here, x goes out of scope, then s. But because s's value was moved, nothing
+        // special happens.
+
+    {
+        let _s1 = gives_ownership();         // gives_ownership moves its return
+            // value into s1
+
+        let s2 = String::from("hello");     // s2 comes into scope
+
+        let _s3 = takes_and_gives_back(s2);     // s2 is moved into
+                                                                // takes_and_gives_back, which also
+                                                                // moves its return value into s3
+    } // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+    // happens. s1 goes out of scope and is dropped.
+
+    {
+        let s1 = String::from("hello");
+
+        let (s1, len) = calculate_length(s1);
+
+        println!("The length of '{}' is {}.", s1, len);
+
+    }
+}
+
+fn takes_ownership(string: String) {
+    println!("{}", string);
+}// Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(integer: i32) {
+    println!("{}", integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
+
+fn gives_ownership() -> String {                // gives_ownership will move its
+                                                // return value into the function
+                                                // that calls it
+
+let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string                             // some_string is returned and
+                                            // moves out to the calling
+                                            // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String {   // a_string comes into
+                                                        // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
 }
