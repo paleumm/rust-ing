4275c802b4a0f301aa77ec2c8fdba8d97d138e0c
paleumm <zdotqdot@gmail.com>
2022-08-21 21:51:21 +0700

    Update main.rs

--- rustl-ing/ownership-concepts/slice/src/main.rs
+++ rustl-ing/ownership-concepts/slice/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("{} {}", hello, world);
+
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[0..len];
+    let slice = &s[..];
+
+    println!("{slice}");
 }
