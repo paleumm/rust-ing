9f85e607a09ca852564c64216f67c4eea982b5d0
paleumm <zdotqdot@gmail.com>
2022-08-29 14:06:34 +0700

    Update main.rs

--- rustl-ing/minigrep/src/main.rs
+++ rustl-ing/minigrep/src/main.rs
@@ -1,15 +1,16 @@
-use std::{env, fs};
+use ::minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Search\t: {}", query);
-    println!("Filename: {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Unable to read file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
