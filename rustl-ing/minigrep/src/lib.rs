199ce8b54bf57cdf958e7228473aa9c93c535bc8
paleumm <zdotqdot@gmail.com>
2022-08-29 15:51:03 +0700

    Update lib.rs

--- rustl-ing/minigrep/src/lib.rs
+++ rustl-ing/minigrep/src/lib.rs
@@ -1,21 +1,12 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
-    // fn new(args: &[String]) -> Config {
-    //     if args.len() < 3 {
-    //         panic!("Not Enough Arguments");
-    //     }
-    //     let query = args[1].clone();
-    //     let file_path = args[2].clone();
-
-    //     Config { query, file_path }
-    // }
-
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
@@ -23,13 +14,84 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result // vector of string
+}
+
+pub fn search_case_insentitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("Whit text:\n{contents}");
+    let result = if config.ignore_case {
+        search_case_insentitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    // iterate through each element in vector
+    for line in result {
+        println!("{line}");
+    }
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insentitive(query, contents)
+        );
+    }
 }
